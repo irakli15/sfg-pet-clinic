5a79617830a0d78880da2149f4a600a9eab4a320
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2021-09-11 23:13:12 +0400

    set up multi-module project

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+//@SpringBootTest("SfgPetClinicApplication")
+@ContextConfiguration
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
