44e3b15314a386f9319bfad681ff8b8c693eb98e
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2021-09-17 23:34:16 +0400

    added specialty

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
+
+public class Vet extends Person {
+	private Set<Specialty> specialty;
+
+	public Set<Specialty> getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Set<Specialty> specialty) {
+		this.specialty = specialty;
+	}
 }
