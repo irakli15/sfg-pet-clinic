e1fed7d130bc48d761cd6078ab1c4003c35a5708
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2021-09-17 23:52:07 +0400

    added pettype and services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+}
